cf195fef9b0e8c5377d2002a7387bd024f815eef
cloudmate <cloudmate@cloudmateui-MacBook-Pro.local>
2023-01-03 17:18:15 +0900

    wwwc2

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id   = "fcceaa7b-16b7-42ca-9b17-0e0c052a9aa1"
+  tenant_id         = "6d5ac8ee-3862-4452-93e7-a836c2d9742b"
+  client_id         = "42526a63-f45c-4d81-868d-54c54b1abaed"
+  client_secret     = "42526a63-f45c-4d81-868d-54c54b1abaed"
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "wc"
+  location = "West Europe"
+}
