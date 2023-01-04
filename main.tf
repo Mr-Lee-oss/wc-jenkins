0323b0407b61033f8aa9990738aa15e69a126bdb
Mr-Lee-oss <65433783+Mr-Lee-oss@users.noreply.github.com>
2023-01-04 14:07:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   subscription_id   = "fcceaa7b-16b7-42ca-9b17-0e0c052a9aa1"
   tenant_id         = "6d5ac8ee-3862-4452-93e7-a836c2d9742b"
   client_id         = "42526a63-f45c-4d81-868d-54c54b1abaed"
-  client_secret     = "42526a63-f45c-4d81-868d-54c54b1abaed"
+  client_secret     = "4NZ8Q~qFcwpm42aeUB-W.TBNM34OFeqtH_6rlbl6"
 }
 
 
